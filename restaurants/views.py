d7cea10fe87ca5ead2fd7caf5f6fa792e27043b8
nalrabah <nalrabah@hotmail.com>
2019-01-24 18:48:38 +0300

    Nourah and Rahmad

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Restaurant
-
+from .forms import RestaurantForm
 def welcome(request):
     return render(request, 'index.html', {'msg':'Hello World!'})
 
@@ -18,8 +18,14 @@ def restaurant_detail(request, restaurant_id):
     return render(request, 'detail.html', context)
 
 def restaurant_create(request):
-
+    restaurant_form = RestaurantForm()
+    if request.method == "POST":
+        restaurant_form = RestaurantForm(request.POST)
+        if restaurant_form.is_valid():
+            restaurant_form.save()
+            return redirect("restaurant-list")
     context = {
+        "form": restaurant_form,
 
     }
     return render(request, 'create.html', context)
